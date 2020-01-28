9f0607f62a7830f94447eb9d0ab5fe5241daafe2
菅原　駿 <sugawarashun@sugawarashunnoMacBook-Air.local>
2020-01-28 16:41:47 +0900

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+
+def top
+end
+
   def index
   	@books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -12,30 +17,39 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
+  	@book = Book.new(book_params)
+  	if @book.save
+      flash[:notice] = "Book was successfully created"
+  	redirect_to @book
+  else @books = Book.all
+    render :index
+  end
+end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+  	redirect_to book_path(@book)
+  else
+    render :edit
+  end
+end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+    flash[:notice] = "Book was successfully destroyed."
   	redirect_to books_path
   end
 
   private
   def book_params
-  	params.require(:book).permit(:title, :body)
-  end
-
-  def edit
-  	@book = Book.find(params[:id])
-  end
-
-  def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book)
-  end
-
-  def destroy
-  	book = Book.find(params[:id])
-  	book.destroy
-  	redirect_to books_path
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :books
+
+ get 'top' => 'books#home'
+
+ root :to => 'books#home'
+
 end
